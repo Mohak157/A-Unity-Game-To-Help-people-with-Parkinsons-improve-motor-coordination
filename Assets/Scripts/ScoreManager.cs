cf1a5bcc796f97371e4f1af0ae239fd570fc5194
Mohak157 <girimohak73@gmail.com>
2026-02-10 22:44:05 +0545

    Added score system

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    private float score;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Player")!= null)
+        {
+            score +=1*Time.deltaTime;
+            scoreText.text = ((int)score).ToString();
+        }
+    }
+}
