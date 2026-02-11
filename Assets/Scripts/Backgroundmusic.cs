eacae824e510e97f9b2e3a2304c0e69ca5f97022
Mohak157 <girimohak73@gmail.com>
2026-02-11 12:16:27 +0545

    added background music

---
+++ Assets/Scripts/Backgroundmusic.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Backgroundmusic : MonoBehaviour
+{
+    private static Backgroundmusic backgroundMusic;
+  void Awake(){
+     if (backgroundMusic != null){
+        Destroy(gameObject);
+
+     }
+     else{
+        backgroundMusic = this;
+        DontDestroyOnLoad(gameObject);
+     }
+  }
+}
