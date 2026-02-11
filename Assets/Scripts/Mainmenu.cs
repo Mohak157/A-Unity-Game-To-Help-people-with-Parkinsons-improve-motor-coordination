e5c803f92149e72fd9fd97d94678758fe90fcd7a
Mohak157 <girimohak73@gmail.com>
2026-02-11 12:51:55 +0545

    Added menu system

---
+++ Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Mainmenu : MonoBehaviour
+{
+   public void PlayGame(){
+    SceneManager.LoadSceneAsync(1);
+   }
+
+   public void QuitGame(){
+    Application.Quit();
+   }
+}
